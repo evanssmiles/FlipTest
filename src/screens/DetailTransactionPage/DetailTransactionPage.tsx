687bd8c8dcf8e3c4aae826723849ba2480d3fb72
Naufal Hendrawan <naufal.hendrawan@ruparupa.com>
2024-11-23 15:38:03 +0700

    Initial commit

---
+++ src/screens/DetailTransactionPage/DetailTransactionPage.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const DetailTransactionPage = () => {
+  return (
+    <View>
+      <Text>DetailTransactionPage</Text>
+    </View>
+  )
+}
+
+export default DetailTransactionPage
+
+// const styles = StyleSheet.create({})
---
+++ src/screens/TransactionListPage/TransactionListPage.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const TransactionListPage = () => {
+  return (
+    <View>
+      <Text>TransactionListPage</Text>
+    </View>
+  );
+};
+
+export default TransactionListPage;
+
+// const styles = StyleSheet.create({})
